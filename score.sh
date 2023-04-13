04a5be0d5dffb45b4d38ab3c303169fabd676773
PS <>
2023-04-13 16:34:57 -0700

    Update score.sh file

--- score.sh
+++ score.sh
@@ -1,6 +1,3 @@
- The candidate seems to have passed following:
- * create file
- * delete file
-Candidate has failed:
- * Modify file.
-FS_SCORE:60%
+echo "This custom script can run any bash command, and perform tests."
+echo "It needs to only output on1 line in the format 'FS_SCORE:xx%', where xx is the percentage score for the solution."
+echo "FS_SCORE:70%" 
