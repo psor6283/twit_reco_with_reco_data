9ca432ebf888637b5ae93e008a1a39825e6e23ae
psor6283 <129886281+psor6283@users.noreply.github.com>
2023-04-18 12:46:55 -0700

    Update metrics documentation
    Remove documentation

--- the-algorithm-ml/metrics/rce.py
+++ the-algorithm-ml/metrics/rce.py
@@ -52,76 +52,6 @@ def _binary_cross_entropy_with_clipping(
 
 
 class RCE(torchmetrics.Metric):
-  """
-  Compute the relative cross entropy (`RCE <http://go/rce>`_).
-
-  RCE is metric used for models predicting probability of success (p), i.e. pCTR.
-  RCE represents the binary `cross entropy <https://en.wikipedia.org/wiki/Cross_entropy>` of
-  the model compared to a reference straw man model.
-
-  Binary cross entropy is defined as:
-
-  y = label; p = prediction;
-  binary cross entropy(example) = - y * log(p) - (1-y) * log(1-p)
-
-  Where y in {0, 1}
-
-  Cross entropy of a model is defined as:
-
-  CE(model) = average(binary cross entropy(example))
-
-  Over all the examples we aggregate on.
-
-  The straw man model is quite simple, it is a constant predictor, always predicting the average
-  over the labels.
-
-  RCE of a model is defined as:
-
-  RCE(model) = 100 * (CE(reference model) - CE(model)) / CE(reference model)
-
-  .. note:: Maximizing the likelihood is the same as minimizing the cross entropy or maximizing
-            the RCE. Since cross entropy is the average minus likelihood for the binary case.
-
-  .. note:: Binary cross entropy of an example is non negative, and equal to the
-            `KL divergence <(https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence
-            #Properties>`
-            since p is constant, and its entropy is equal to zero.
-
-  .. note:: 0% RCE means as good as the straw man model.
-            100% means always predicts exactly the label. Namely, cross entropy of the model is
-                always zero. In practice 100% is impossible to achieve due to clipping.
-            Negative RCE means that the model is doing worse than the straw man.
-            This usually means an un-calibrated model, namely, the average prediction
-            is "far" from the average label. Examining NRCE might help identifying if that is
-            the case.
-
-  .. note:: RCE is not a "ratio" in the statistical
-            `level of measurement sense <https://en.wikipedia.org/wiki/Level_of_measurement>`.
-            The higher the model's RCE is the harder it is to improve it by an extra point.
-
-            For example:
-            Let CE(model) = 0.5 CE(reference model), then the RCE(model) = 50.
-            Now take a "twice as good" model:
-            Let CE(better model) = 0.5 CE(model) = 0.25 CE(reference model),
-            then the RCE(better model) = 75 and not 100.
-
-  .. note:: In order to keep the log function stable, typically p is limited to
-            lie in [CLAMP_EPSILON, 1-CLAMP_EPSILON],
-            where CLAMP_EPSILON is some small constant like: 1e-7.
-            Old implementation used 1e-5 clipping by default, current uses
-            tf.keras.backend.epsilon()
-            whose default is 1e-7.
-
-  .. note:: Since the reference model prediction is constant (probability),
-            CE(reference model) = H(average(label))
-
-            Where H is the standard
-            `entropy <https://en.wikipedia.org/wiki/Entropy_(information_theory)>` function.
-
-  .. note:: Must have at least 1 positive and 1 negative sample accumulated,
-            or RCE will come out as NaN.
-  """
-
   def __init__(
     self, from_logits: bool = False, label_smoothing: float = 0, epsilon: float = 1e-7, **kwargs
   ):
@@ -203,26 +133,6 @@ class RCE(torchmetrics.Metric):
 
 
 class NRCE(RCE):
-  """
-  Calculate the RCE of the normalizes model.
-  Where the normalized model prediction average is normalized to the average label seen so far.
-  Namely, the the normalized model prediction:
-
-  normalized model prediction(example) = (model prediction(example) * average(label)) /
-  average(model prediction)
-
-  Where the average is over all previously seen examples.
-
-  .. note:: average(normalized model prediction) = average(label)
-
-  .. note:: NRCE can be misleading since it is oblivious to mis-calibrations.
-            The common interpretation of NRCE is to measure how good your model could potentially
-            perform if it was well calibrated.
-
-  .. note:: A big gap between NRCE and RCE might indicate a badly calibrated model,
-
-  """
-
   def __init__(
     self, from_logits: bool = False, label_smoothing: float = 0, epsilon: float = 1e-7, **kwargs
   ):
